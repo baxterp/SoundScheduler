3d720cc2e6f8a0f85a0199431f6a095e60e5a44b
Baxter Pearson <baxterrpearson@gmail.com>
2020-07-07 13:57:50 +0100

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace SoundScheduler
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.soundEventsDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.soundEventsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // soundEventsDataGridView
+            // 
+            this.soundEventsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.soundEventsDataGridView.Location = new System.Drawing.Point(28, 38);
+            this.soundEventsDataGridView.Name = "soundEventsDataGridView";
+            this.soundEventsDataGridView.Size = new System.Drawing.Size(1081, 443);
+            this.soundEventsDataGridView.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 748);
+            this.Controls.Add(this.soundEventsDataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Sound Scheduler";
+            ((System.ComponentModel.ISupportInitialize)(this.soundEventsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView soundEventsDataGridView;
+    }
+}
+
