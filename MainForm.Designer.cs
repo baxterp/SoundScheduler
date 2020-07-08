50302af35ed8916264e14ca41bb8d9d5514b6152
Baxter Pearson <baxterrpearson@gmail.com>
2020-07-08 16:11:05 +0100

    All working bar logging to DB

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -31,8 +31,8 @@
             this.soundEventsDataGridView = new System.Windows.Forms.DataGridView();
             this.eventsDateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.gbSelectDate = new System.Windows.Forms.GroupBox();
+            this.gbCreateNewEvents = new System.Windows.Forms.GroupBox();
             this.startCreateNewEventsBtn = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.newEventsDateTimePicker = new System.Windows.Forms.DateTimePicker();
@@ -42,19 +42,27 @@
             this.endTimeCmb = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.startTimeCmb = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnStopSheduler = new System.Windows.Forms.Button();
+            this.btnStartSheduler = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.rtbMessages = new System.Windows.Forms.RichTextBox();
+            this.btnPlaySelectedRowSound = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.soundEventsDataGridView)).BeginInit();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
+            this.gbSelectDate.SuspendLayout();
+            this.gbCreateNewEvents.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // soundEventsDataGridView
             // 
             this.soundEventsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.soundEventsDataGridView.Location = new System.Drawing.Point(41, 49);
+            this.soundEventsDataGridView.Location = new System.Drawing.Point(27, 68);
             this.soundEventsDataGridView.Name = "soundEventsDataGridView";
-            this.soundEventsDataGridView.Size = new System.Drawing.Size(340, 341);
+            this.soundEventsDataGridView.Size = new System.Drawing.Size(350, 413);
             this.soundEventsDataGridView.TabIndex = 0;
+            this.soundEventsDataGridView.SelectionChanged += new System.EventHandler(this.soundEventsDataGridView_SelectionChanged);
             // 
             // eventsDateTimePicker
             // 
@@ -62,45 +70,48 @@
             this.eventsDateTimePicker.Name = "eventsDateTimePicker";
             this.eventsDateTimePicker.Size = new System.Drawing.Size(200, 20);
             this.eventsDateTimePicker.TabIndex = 1;
-            this.eventsDateTimePicker.Value = new System.DateTime(2020, 7, 7, 14, 40, 14, 0);
+            this.eventsDateTimePicker.Value = new System.DateTime(2020, 7, 8, 0, 0, 0, 0);
             this.eventsDateTimePicker.ValueChanged += new System.EventHandler(this.eventsDateTimePicker_ValueChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 30);
+            this.label1.Location = new System.Drawing.Point(24, 52);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.Size = new System.Drawing.Size(160, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Sound Events for Selected Day";
+            this.label1.Text = "Sound Events for Selected Date";
             // 
-            // groupBox1
+            // gbSelectDate
             // 
-            this.groupBox1.Controls.Add(this.eventsDateTimePicker);
-            this.groupBox1.Location = new System.Drawing.Point(402, 30);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(264, 230);
-            this.groupBox1.TabIndex = 3;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Select Date";
+            this.gbSelectDate.Controls.Add(this.btnPlaySelectedRowSound);
+            this.gbSelectDate.Controls.Add(this.eventsDateTimePicker);
+            this.gbSelectDate.Controls.Add(this.soundEventsDataGridView);
+            this.gbSelectDate.Controls.Add(this.label1);
+            this.gbSelectDate.Location = new System.Drawing.Point(695, 27);
+            this.gbSelectDate.Name = "gbSelectDate";
+            this.gbSelectDate.Size = new System.Drawing.Size(396, 578);
+            this.gbSelectDate.TabIndex = 3;
+            this.gbSelectDate.TabStop = false;
+            this.gbSelectDate.Text = "Select Date";
             // 
-            // groupBox2
+            // gbCreateNewEvents
             // 
-            this.groupBox2.Controls.Add(this.startCreateNewEventsBtn);
-            this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Controls.Add(this.newEventsDateTimePicker);
-            this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Controls.Add(this.numberCmb);
-            this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Controls.Add(this.endTimeCmb);
-            this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Controls.Add(this.startTimeCmb);
-            this.groupBox2.Location = new System.Drawing.Point(41, 421);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(655, 181);
-            this.groupBox2.TabIndex = 4;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Create new events";
+            this.gbCreateNewEvents.Controls.Add(this.startCreateNewEventsBtn);
+            this.gbCreateNewEvents.Controls.Add(this.label5);
+            this.gbCreateNewEvents.Controls.Add(this.newEventsDateTimePicker);
+            this.gbCreateNewEvents.Controls.Add(this.label4);
+            this.gbCreateNewEvents.Controls.Add(this.numberCmb);
+            this.gbCreateNewEvents.Controls.Add(this.label3);
+            this.gbCreateNewEvents.Controls.Add(this.endTimeCmb);
+            this.gbCreateNewEvents.Controls.Add(this.label2);
+            this.gbCreateNewEvents.Controls.Add(this.startTimeCmb);
+            this.gbCreateNewEvents.Location = new System.Drawing.Point(23, 203);
+            this.gbCreateNewEvents.Name = "gbCreateNewEvents";
+            this.gbCreateNewEvents.Size = new System.Drawing.Size(655, 136);
+            this.gbCreateNewEvents.TabIndex = 4;
+            this.gbCreateNewEvents.TabStop = false;
+            this.gbCreateNewEvents.Text = "Create new events";
             // 
             // startCreateNewEventsBtn
             // 
@@ -145,8 +156,12 @@
             "1",
             "2",
             "3",
-            "4"});
-            this.numberCmb.Location = new System.Drawing.Point(377, 37);
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.numberCmb.Location = new System.Drawing.Point(377, 36);
             this.numberCmb.Name = "numberCmb";
             this.numberCmb.Size = new System.Drawing.Size(121, 21);
             this.numberCmb.TabIndex = 4;
@@ -215,37 +230,88 @@
             this.startTimeCmb.TabIndex = 0;
             this.startTimeCmb.SelectedIndexChanged += new System.EventHandler(this.startTimeCmb_SelectedIndexChanged);
             // 
-            // button1
+            // groupBox3
             // 
-            this.button1.Location = new System.Drawing.Point(429, 646);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(161, 58);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.groupBox3.Controls.Add(this.btnStopSheduler);
+            this.groupBox3.Controls.Add(this.btnStartSheduler);
+            this.groupBox3.Location = new System.Drawing.Point(23, 27);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(564, 151);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Shedule Events";
+            // 
+            // btnStopSheduler
+            // 
+            this.btnStopSheduler.Location = new System.Drawing.Point(252, 45);
+            this.btnStopSheduler.Name = "btnStopSheduler";
+            this.btnStopSheduler.Size = new System.Drawing.Size(163, 58);
+            this.btnStopSheduler.TabIndex = 1;
+            this.btnStopSheduler.Text = "Stop Scheduler";
+            this.btnStopSheduler.UseVisualStyleBackColor = true;
+            this.btnStopSheduler.Click += new System.EventHandler(this.btnStopSheduler_Click);
+            // 
+            // btnStartSheduler
+            // 
+            this.btnStartSheduler.Location = new System.Drawing.Point(34, 45);
+            this.btnStartSheduler.Name = "btnStartSheduler";
+            this.btnStartSheduler.Size = new System.Drawing.Size(163, 58);
+            this.btnStartSheduler.TabIndex = 0;
+            this.btnStartSheduler.Text = "Start Scheduler";
+            this.btnStartSheduler.UseVisualStyleBackColor = true;
+            this.btnStartSheduler.Click += new System.EventHandler(this.btnStartSheduler_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.rtbMessages);
+            this.groupBox4.Location = new System.Drawing.Point(23, 365);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(655, 240);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Log Messages";
+            // 
+            // rtbMessages
+            // 
+            this.rtbMessages.Location = new System.Drawing.Point(13, 19);
+            this.rtbMessages.Name = "rtbMessages";
+            this.rtbMessages.ReadOnly = true;
+            this.rtbMessages.Size = new System.Drawing.Size(622, 208);
+            this.rtbMessages.TabIndex = 0;
+            this.rtbMessages.Text = "";
+            // 
+            // btnPlaySelectedRowSound
+            // 
+            this.btnPlaySelectedRowSound.Location = new System.Drawing.Point(27, 498);
+            this.btnPlaySelectedRowSound.Name = "btnPlaySelectedRowSound";
+            this.btnPlaySelectedRowSound.Size = new System.Drawing.Size(350, 50);
+            this.btnPlaySelectedRowSound.TabIndex = 3;
+            this.btnPlaySelectedRowSound.Text = "Play selected sound";
+            this.btnPlaySelectedRowSound.UseVisualStyleBackColor = true;
+            this.btnPlaySelectedRowSound.Click += new System.EventHandler(this.btnPlaySelectedRowSound_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(719, 748);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.soundEventsDataGridView);
+            this.ClientSize = new System.Drawing.Size(1116, 631);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.gbCreateNewEvents);
+            this.Controls.Add(this.gbSelectDate);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "Sound Scheduler";
             this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.soundEventsDataGridView)).EndInit();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
+            this.gbSelectDate.ResumeLayout(false);
+            this.gbSelectDate.PerformLayout();
+            this.gbCreateNewEvents.ResumeLayout(false);
+            this.gbCreateNewEvents.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -254,8 +320,8 @@
         private System.Windows.Forms.DataGridView soundEventsDataGridView;
         private System.Windows.Forms.DateTimePicker eventsDateTimePicker;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox gbSelectDate;
+        private System.Windows.Forms.GroupBox gbCreateNewEvents;
         private System.Windows.Forms.ComboBox startTimeCmb;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox endTimeCmb;
@@ -265,7 +331,12 @@
         private System.Windows.Forms.Button startCreateNewEventsBtn;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.DateTimePicker newEventsDateTimePicker;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnStopSheduler;
+        private System.Windows.Forms.Button btnStartSheduler;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RichTextBox rtbMessages;
+        private System.Windows.Forms.Button btnPlaySelectedRowSound;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -9,15 +9,27 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Timers;
 
 namespace SoundScheduler
 {
     public partial class MainForm : Form
     {
-        SoundEventsEntities soundEventsModel = new SoundEventsEntities();
-        public delegate void InvokeDelegate();
-        
-        List<string> endTimeValues = new List<string>();
+        #region Private Members
+
+        private SoundEventsEntities soundEventsModel = new SoundEventsEntities();
+        private delegate void InvokeDelegate();
+        private System.Timers.Timer eventsTimer;
+        private string baseResourceLocation = Environment.CurrentDirectory + @"\Resources\";
+        private int timerFrequency = 60000;
+        private System.Media.SoundPlayer player = null;
+        private string filenameToPlay = string.Empty;
+
+        private List<ScheduledEventsData> scheduledEventsForToday;
+
+        #endregion
+
+        #region Form Events
 
         public MainForm()
         {
@@ -26,12 +38,13 @@ namespace SoundScheduler
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            eventsDateTimePicker.Value = DateTime.Now;
             startTimeCmb.SelectedIndex = 0;
             numberCmb.SelectedIndex = 0;
             endTimeCmb.Enabled = false;
             numberCmb.Enabled = false;
             startCreateNewEventsBtn.Enabled = false;
+            eventsDateTimePicker.Value = DateTime.Now;
+            btnPlaySelectedRowSound.Enabled = false;
         }
 
         private void eventsDateTimePicker_ValueChanged(object sender, EventArgs e)
@@ -134,15 +147,115 @@ namespace SoundScheduler
                 }
                 catch (Exception ex)
                 {
-
+                    SendLogMessage("Problem saving data to the database, message : " + ex.Message);
                 }
 
                 InvokeUIThreadAction(new Action(() =>
                 {
+                    eventsDateTimePicker.Value = new DateTime(2001, 12, 12);
                     eventsDateTimePicker.Value = newEventsDateTimePicker.Value;
                 }));
 
             }));
+        }
+
+        private void btnStartSheduler_Click(object sender, EventArgs e)
+        {
+            InvokeUIThreadAction(new Action(() =>
+            {
+                gbCreateNewEvents.Enabled = false;
+                gbSelectDate.Enabled = false;
+                btnStartSheduler.Enabled = false;
+            }));
+
+            try
+            {
+                eventsTimer = new System.Timers.Timer(timerFrequency);
+                eventsTimer.Elapsed += EventsTimer_Elapsed;
+                eventsTimer.Start();
+
+                scheduledEventsForToday = soundEventsModel.SoundEvents
+                                                .Where(s =>
+                                                        s.TimeStamp.Value.Year == DateTime.Now.Year
+                                                        && s.TimeStamp.Value.Month == DateTime.Now.Month
+                                                        && s.TimeStamp.Value.Day == DateTime.Now.Day
+                                                        && s.IsEnabled == true)
+                                                .Select(s => new ScheduledEventsData
+                                                {
+                                                    EventsTime = s.TimeStamp,
+                                                    FileNameToPlay = s.Sound.SoundFileName
+                                                }).ToList();
+
+                SendLogMessage("Events timer STARTED successfully");
+            }
+            catch (Exception ex)
+            {
+                SendLogMessage("Problem starting events timer: " + ex.Message);
+
+                InvokeUIThreadAction(new Action(() =>
+                {
+                    gbCreateNewEvents.Enabled = true;
+                    gbSelectDate.Enabled = true;
+                    btnStartSheduler.Enabled = true;
+                }));
+            }
+        }
+
+        private void btnStopSheduler_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                eventsTimer.Stop();
+                SendLogMessage("Events timer STOPPED successfully");
+            }
+            catch (Exception ex)
+            {
+                SendLogMessage("Problem stopping events timer: " + ex.Message);
+            }
+
+            InvokeUIThreadAction(new Action(() =>
+            {
+                gbCreateNewEvents.Enabled = true;
+                gbSelectDate.Enabled = true;
+                btnStartSheduler.Enabled = true;
+            }));
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void EventsTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            bool messageSent = false;
+            scheduledEventsForToday.ForEach(scheduledEvent =>
+            {
+                if (scheduledEvent.EventsTime.Value.Minute == DateTime.Now.Minute
+                    && scheduledEvent.EventsTime.Value.Hour == DateTime.Now.Hour)
+                {
+                    var filePath = baseResourceLocation + scheduledEvent.FileNameToPlay;
+
+                    try
+                    {
+                        PlaySound(filePath);
+
+                        SendLogMessage("Sound file : " + scheduledEvent.FileNameToPlay + ", played successfully @ " + DateTime.Now.ToString("dd MMMM HH:mm"));
+                        messageSent = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        SendLogMessage("Sound file : " + scheduledEvent.FileNameToPlay + ", FAILED to play @ " + DateTime.Now.ToString("dd MMMM HH:mm"));
+                        SendLogMessage("Error details: " + ex.Message);
+                        messageSent = true;
+                    }
+                }
+            });
+
+            if (!messageSent)
+            {
+                SendLogMessage("No sound to play during timer event @ " + DateTime.Now.ToString("dd MMMM HH:mm"));
+                messageSent = true;
+            }
         }
 
         private void DeleteOldEntriesForDate(DateTime dateToDeleteFor)
@@ -174,14 +287,60 @@ namespace SoundScheduler
             this.BeginInvoke(new InvokeDelegate(actionToExecute));
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void PlaySound(string fileName)
         {
-            var AppPath = Environment.CurrentDirectory;
-
-            var filePath = AppPath + @"\Resources\1.wav";
-
-            System.Media.SoundPlayer player = new System.Media.SoundPlayer(filePath);
-            player.Play();
+            try
+            {
+                player = new System.Media.SoundPlayer(fileName);
+                player.Play();
+            }
+            catch (Exception ex)
+            {
+                SendLogMessage("Problem playing sound file: " + fileName);
+                SendLogMessage("Error: " + ex.Message);
+            }
         }
+
+        private void SendLogMessage(string messageToDisplay)
+        {
+            InvokeUIThreadAction(new Action(() =>
+            {
+                rtbMessages.AppendText("> " + messageToDisplay + Environment.NewLine);
+                rtbMessages.ScrollToCaret();
+            }));
+        }
+
+        #endregion
+
+        private void soundEventsDataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            btnPlaySelectedRowSound.Enabled = false;
+
+            var rowsCount = soundEventsDataGridView.SelectedRows.Count;
+            if (rowsCount == 0 || rowsCount > 1) return;
+
+            var row = soundEventsDataGridView.SelectedRows[0];
+            if (row == null) return;
+
+            var filename = row.Cells[1].Value.ToString();
+
+            filenameToPlay = filename;
+
+            btnPlaySelectedRowSound.Enabled = true;
+        }
+
+        private void btnPlaySelectedRowSound_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(filenameToPlay))
+            {
+                PlaySound(baseResourceLocation +  filenameToPlay);
+            }
+        }
+    }
+
+    public class ScheduledEventsData
+    {
+        public DateTime? EventsTime { get; set; }
+        public string FileNameToPlay { get; set; }
     }
 }
